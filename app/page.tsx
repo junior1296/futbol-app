80ad5e5ee18add8d2837e288e1bdf85c9e9d3a4e
Junior Mejia Osorio <jmejiaosorio.1296@gmail.com>
2024-09-11 22:32:06 -0500

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,172 @@
 import Image from "next/image";
 
-export default function Home() {
+const CHANNELS = [
+  {
+    name: "movistar",
+    stream: "https://la10hd.com/vivo/canal.php?stream=movistar",
+    stream2: "https://la10hd.com/tv/movistar.html",
+  },
+  {
+    name: "liga 1 max",
+    stream: "https://la10hd.com/vivo/canal.php?stream=liga1max",
+    stream2: "https://la10hd.com/tv/liga1max.html",
+  },
+  {
+    name: "espn",
+    stream: "https://la10hd.com/vivo/canal.php?stream=espn",
+    stream2: "https://la10hd.com/tv/espn.html",
+  },
+  {
+    name: "espn 2",
+    stream: "https://la10hd.com/vivo/canal.php?stream=espn2",
+    stream2: "https://la10hd.com/tv/espn2.html",
+  },
+  {
+    name: "espn 3",
+    stream: "https://la10hd.com/vivo/canal.php?stream=espn3",
+    stream2: "https://la10hd.com/tv/espn3.html",
+  },
+  {
+    name: "espn premium",
+    stream: "https://la10hd.com/vivo/canal.php?stream=espnpremium",
+    stream2: "https://la10hd.com/tv/espnpremium.html",
+  },
+  {
+    name: "dsports",
+    stream: "https://la10hd.com/vivo/canal.php?stream=dsports",
+    stream2: "https://la10hd.com/tv/dsports.html",
+  },
+  {
+    name: "dsports 2",
+    stream: "https://la10hd.com/vivo/canal.php?stream=dsports2",
+    stream2: "https://la10hd.com/tv/dsports2.html",
+  },
+  {
+    name: "dsports+",
+    stream: "https://la10hd.com/vivo/canal.php?stream=dsportsplus",
+    stream2: "https://la10hd.com/tv/dsportsplus.html",
+  },
+  {
+    name: "caracol tv",
+    stream: "https://la10hd.com/vivo/canal.php?stream=caracol",
+    stream2: "https://la10hd.com/tv/caracol.html",
+  },
+  {
+    name: "win sports",
+    stream: "https://la10hd.com/vivo/canal.php?stream=winsports",
+    stream2: "https://la10hd.com/tv/winsports.html",
+  },
+  {
+    name: "win sports +",
+    stream: "https://la10hd.com/vivo/canal.php?stream=winsportsplus",
+    stream2: "https://la10hd.com/tv/winsportsplus.html",
+  },
+  {
+    name: "gol peru",
+    stream: "https://la10hd.com/vivo/canal.php?stream=golperu",
+    stream2: "https://la10hd.com/tv/golperu.html",
+  },
+  {
+    name: "premiere br",
+    stream: "https://golazohd.com/mpd.php?id=premiere",
+    stream2: "https://golazohd.com/mpd.php?id=premiere",
+  },
+  {
+    name: "fox sports",
+    stream: "https://golazohd.com/mpd.php?id=premiere",
+    stream2: "https://la10hd.com/tv/foxsports.html",
+  },
+  {
+    name: "fox sports 2",
+    stream: "https://golazohd.com/mpd.php?id=foxsports2",
+    stream2: "https://la10hd.com/tv/foxsports2.html",
+  },
+  {
+    name: "fox sports 3",
+    stream: "https://golazohd.com/mpd.php?id=foxsports3",
+    stream2: "https://la10hd.com/tv/foxsports3.html",
+  },
+  {
+    name: "tnt sports",
+    stream: "https://golazohd.com/mpd.php?id=tntsports",
+    stream2: "https://la10hd.com/tv/tntsports.html",
+  }
+]
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+const getVideo = (embed: string) => {
+  const value: string = embed.toLowerCase().trim()
+  const url_video: string = 'https://la10hd.com/vivo/canal.php?stream='
+
+  return CHANNELS.find(stream => stream.name == value)?.stream
+}
+
+export default async function Home() {
+  let URL_BASE = 'https://futbollibrehd.pe'
+  let data = await fetch('https://corsproxy.io/?url=https://futbollibrehd.pe/agenda.json')
+  let posts = await data.json()
+  
+  let URL_CHANNEL = 'https://la10hd.com/vivo/canal.php?stream='
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="row-start-1">
+        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+          <div className="flex gap-x-12">
+            {/* {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-300">
+                {item.name}
+              </a>
+            ))} */}
+            <a href="#" className="text-lg font-semibold leading-1 text-gray-300">
+              Futbol App
+            </a>
+          </div>
+        </nav>
+      </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <ul role="list" className="divide-y divide-gray-100">
+          {posts.data.map((post) => (
+            <li key={post.id} className="flex flex-col justify-between gap-x-6 gap-y-6 py-5">
+              <div className="flex flex-row gap-x-8 justify-between">
+                <p>{post.attributes.diary_description}</p>
+                <div className="flex flex-row gap-8">
+                  <p>{post.attributes.date_diary}</p>
+                  <p>{post.attributes.diary_hour}</p>
+                </div>
+              </div>
+              <ul className="flex flex-row gap-x-8">
+                {post.attributes.embeds.data.map((embed) => (
+                  <li key={embed.attributes.embed_name} className="flex items-center gap-x-6 bg-gray-600 rounded">
+                    <a href={getVideo(embed.attributes.embed_name)} target="_blank" className="p-2">
+                      {embed.attributes.embed_name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <aside className="row-start-3 flex flex-row gap-6 items-center justify-center">
+          <ul className="flex flex-row gap-8">
+            {CHANNELS.map((embed) => (
+              <li key={embed.name} className="flex items-center gap-x-6 bg-gray-600 rounded">
+                <a href={embed.stream} target="_blank" className="p-2">
+                  {embed.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+      </aside>
+      <footer className="row-start-4 flex gap-6 flex-wrap items-center justify-center">
+        <p>Hecho por Junior Mejia Osorio</p>
       </footer>
     </div>
   );
