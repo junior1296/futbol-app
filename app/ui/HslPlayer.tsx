ca40bfe8fe45223bee6480656209f3929ed1161e
Junior Mejia Osorio <jmejiaosorio.1296@gmail.com>
2024-09-16 14:07:00 -0500

    fix videoRef when was null

--- app/ui/HslPlayer.tsx
+++ app/ui/HslPlayer.tsx
@@ -7,15 +7,17 @@ export default function VideoPlayer({src}: {src: string}) {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    const video = videoRef.current;
-
-    if (Hls.isSupported()) {
-      const hls = new Hls();
-      hls.loadSource(src);
-      hls.attachMedia(video);
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-      // Para navegadores que soportan HLS nativo
-      video.src = src;
+    if (videoRef.current) {
+    const video: HTMLMediaElement = videoRef.current;
+    
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(video);
+      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        // Para navegadores que soportan HLS nativo
+        video.src = src;
+      }
     }
   }, [src]);
 
